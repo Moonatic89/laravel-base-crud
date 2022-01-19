c53c6c3dd4df29bc45def130cb385ab2a9f3ad67
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-01-19 15:39:19 +0100

    FEAT - Add Comic Seeder, add text DB

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
