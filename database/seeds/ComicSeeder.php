384ab6e24b11a0a6d3fb742d3927c40864a1aca1
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-01-19 15:53:43 +0100

    FEAT - DB seeded

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -11,5 +12,17 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
+
+        $comics = config('dbcomics.comics');
+
+        foreach ($comics as $comic) {
+            $_comic = new Comic();
+            $_comic->title = $comic['title'];
+            $_comic->description = $comic['description'];
+            $_comic->thumb = $comic['thumb'];
+            $_comic->price = $comic['price'];
+            $_comic->series = $comic['series'];
+            $_comic->save();
+        }
     }
 }
