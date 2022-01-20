95695e038c75ff7e03248c4ea1e444524543cc09
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2022-01-20 08:14:33 +0100

    FEAT - Added a button to enter create view

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,8 +4,15 @@
 @section ('content')
 
 <div class="container mt-5">
-    <div class="row row-cols-4">
+
+    <a name="create" id="create" class="btn btn-primary text-center" href="{{route('admin.comics.create')}}"
+        role="button">Click
+        here to add a new Comic</a>
+
+
+    <div class="row row-cols-4 mt-3">
         @foreach($comics as $comic)
+
         <div class="col">
             <div class="card" style="width: 18rem;">
                 <img src="{{$comic->thumb}}" class="card-img-top w-100" alt="...">
